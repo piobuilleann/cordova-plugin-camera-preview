632bc963e3b83878c410cbbf8b78d8cc2d18018e
Christian <christian@springscript.com>
2017-05-31 11:01:05 -0500

    added diff version of OpenCV

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -292,7 +292,7 @@ public class CameraActivity extends Fragment {
     super.onResume();
 
 	//christian
-	OpenCVLoader.initAsync( OpenCVLoader.OPENCV_VERSION_3_2_0, getActivity(), mLoaderCallback );
+	OpenCVLoader.initAsync( OpenCVLoader.OPENCV_VERSION_2_4_8, getActivity(), mLoaderCallback );
 	
 	
     mCamera = Camera.open(defaultCameraId);
