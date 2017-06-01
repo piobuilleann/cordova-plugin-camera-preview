678824fee02b1b87ca614e67784b107849eaaa48
Christian <christian@springscript.com>
2017-06-01 11:05:15 -0500

    added calback ref

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -45,7 +45,32 @@ import java.util.Date;
 import java.util.List;
 import java.util.Arrays;
 
-public class CameraActivity extends Fragment {
+
+
+
+//Christian
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfDMatch;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+
+//christian
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+
+public class CameraActivity extends Fragment implements CvCameraViewListener2 {
 
   public interface CameraPreviewListener {
     void onPictureTaken(String originalPicture);
