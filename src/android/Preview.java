e3c8fe9a64a74c25544be3a9f40040708a2517ff
Christian <christian@springscript.com>
2017-06-02 11:34:45 -0500

    adding texture vars

--- src/android/Preview.java
+++ src/android/Preview.java
@@ -1,29 +1,18 @@
 package com.cordovaplugincamerapreview;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.ImageFormat;
-import android.graphics.Rect;
-import android.graphics.YuvImage;
-import android.hardware.Camera;
-//import android.support.v8.renderscript.RenderScript;
-import android.util.DisplayMetrics;
-import android.util.Log;
-
-//import android.view.Surface;
-//import android.view.SurfaceHolder;
-import android.graphics.SurfaceTexture;
-import android.view.TextureView;
-
-import android.view.View;
-import android.widget.RelativeLayout;
-import org.apache.cordova.LOG;
-
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.List;
 
-
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.TextureView;
+import android.view.TextureView.SurfaceTextureListener;
+import android.view.View;
+import android.widget.FrameLayout;
 
 class Preview extends RelativeLayout implements TextureView.SurfaceTextureListener {
   private final String TAG = "Preview";
