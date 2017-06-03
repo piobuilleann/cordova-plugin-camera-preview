03d808e1b29112c0c67a4dd9d5aff9c22d7cd113
Christian <christian@springscript.com>
2017-06-03 15:25:06 -0500

    extends activity

--- src/android/Preview.java
+++ src/android/Preview.java
@@ -95,7 +95,7 @@ class Preview extends Activity implements TextureView.SurfaceTextureListener {
         // Update your view here!
     }  
 
-    public getView()
+    public TextureView getView()
 	{
 		return mTextureView;
 	}
