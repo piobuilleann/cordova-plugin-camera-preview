16d1a6f99cd0462d6eef7bb60488d58802b3e5ef
Christian <christian@springscript.com>
2017-06-03 14:53:46 -0500

    extends activity

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -14,6 +14,7 @@ import android.graphics.ImageFormat;
 import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.ShutterCallback;
