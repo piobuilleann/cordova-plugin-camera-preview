e22654f48b9ccc84ca584fe04d14a4853f6b8dae
Christian <christian@springscript.com>
2017-06-03 13:09:00 -0500

    remove return

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -142,7 +142,7 @@ public class CameraActivity extends Fragment implements Camera.PreviewCallback  
       frameContainerLayout.setLayoutParams(layoutParams);
 
       //video view
-      TextureView mPreview = new Preview(getActivity());
+      mPreview = new Preview(getActivity());
 	  //webView.loadUrl("javascript:console.log('" + mPreview + "');");
       mainLayout = (FrameLayout) view.findViewById(getResources().getIdentifier("video_view", "id", appResourcesPackage));
       mainLayout.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
