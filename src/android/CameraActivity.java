e4924923eeaa805aa615bb4d5926476d53a33571
Christian <christian@springscript.com>
2017-06-09 11:27:25 -0500

    added context

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -171,20 +171,16 @@ public class CameraActivity extends Fragment implements TextureView.SurfaceTextu
         super.onAttach(context);
 
 		
-		Activity a;
 
-		if (context instanceof Activity){
-			a=(Activity) context;
 
-			mTextureView = (TextureView) view.findViewById(getResources().getIdentifier("textureView1", "id", appResourcesPackage));
-			
-			filter = new Filter(RenderScript.create( a ));
-			
-			mTextureView.setSurfaceTextureListener(filter);
-			
-			Log.d("onCreateView", "create new texture view");	
-			createCameraPreview();		
-		}		
+		mTextureView = (TextureView) view.findViewById(getResources().getIdentifier("textureView1", "id", appResourcesPackage));
+		
+		filter = new Filter(RenderScript.create( context ));
+		
+		mTextureView.setSurfaceTextureListener(filter);
+		
+		Log.d("onCreateView", "create new texture view");	
+		createCameraPreview();		
 		
 		
     }  
