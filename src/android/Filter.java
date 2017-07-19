61f19849f663ee359ee57b2854579a03245590c6
Christian <christian@springscript.com>
2017-07-19 10:35:47 -0500

    thresholds going in is now comming out for testing

--- src/android/Filter.java
+++ src/android/Filter.java
@@ -121,10 +121,15 @@ public class Filter implements TextureView.SurfaceTextureListener {
 
     public void execute(byte[] yuv) {
         if (mHaveSurface) {
-            mAllocationIn.copy1DRangeFrom(0, mSize, yuv);
+            //mAllocationIn.copy1DRangeFrom(0, mSize, yuv);
 
             if (blending == 0) {
-                mEffects.forEach_copy(mAllocationIn, mAllocationOut);
+				//
+				mAllocationIn.copyFrom(yuv);
+				mAllocationOut.copyFrom(mAllocationIn);
+				//
+				
+                //mEffects.forEach_copy(mAllocationIn, mAllocationOut);
             } else {
                 mHistogram.forEach_Dot(mAllocationIn);
                 mAllocationHistogram.copyTo(histo);
