d17c80551623fd3d1c7570591ebe75a5b953ed93
Christian <christian@springscript.com>
2017-07-19 15:46:15 -0500

    thresholds going in is now comming out for testing

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -147,7 +147,7 @@ public class CameraActivity extends Fragment implements TextureView.SurfaceTextu
   
   
 	@Override
-	public void onPreviewFrame(byte[]... data, Camera c) {
+	public void onPreviewFrame(byte[] data, Camera c) {
         if (isProcessing || state != STATE_PREVIEW) {
             mCamera.addCallbackBuffer(data);
             return;
@@ -168,9 +168,7 @@ public class CameraActivity extends Fragment implements TextureView.SurfaceTextu
 		//new ProcessPreviewDataTask().execute(data);		
 		
 		//
-		byte[] datas = data[0];
-		filter.execute( datas );
-        mCamera.addCallbackBuffer( datas );		
+        mCamera.addCallbackBuffer( data );		
 		//
 	}
   
