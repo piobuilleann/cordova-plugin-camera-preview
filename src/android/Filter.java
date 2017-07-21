17f78e5b39ddc5064fb29410abce6dfcfa66a5b5
Christian <christian@springscript.com>
2017-07-21 09:27:02 -0500

    removed filter for testing

--- src/android/Filter.java
+++ src/android/Filter.java
@@ -121,12 +121,12 @@ public class Filter implements TextureView.SurfaceTextureListener {
 
     public void execute(byte[] yuv) {
         if (mHaveSurface) {
-            //mAllocationIn.copy1DRangeFrom(0, mSize, yuv);
+            mAllocationIn.copy1DRangeFrom(0, mSize, yuv);
 
             if (blending == 0) {
                 //mEffects.forEach_copy(mAllocationIn, mAllocationOut);
-				mAllocationIn.copyFrom(yuv);
 				mAllocationOut.copyFrom(mAllocationIn);
+				//mAllocationOut.copyFrom(mAllocationIn);
             } else {
                 mHistogram.forEach_Dot(mAllocationIn);
                 mAllocationHistogram.copyTo(histo);
