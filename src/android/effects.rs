0aaac05386a902e99fbeed5ab43f765b52b3f53f
Christian <christian@springscript.com>
2017-07-22 16:10:41 -0500

    removed filter for testing

--- src/android/effects.rs
+++ src/android/effects.rs
@@ -258,8 +258,8 @@ uchar4 __attribute__((kernel)) kMeans(uchar4 in, uint32_t x, uint32_t y) {
 	//uchar4 result;
 	
 	//float pixel = getElementAt_uchar_to_float(in, x, y);
-	uchar4 f4 = convert_char4(in);
-	rsDebug("float4: ", f4);
+	//uchar4 f4 = convert_char4(in);
+	rsDebug("float4: ", in);
 	
 	//return mAllocationTest;
 }
