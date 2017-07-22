8243cfeae53d0792a22447f4fb2d5308d72bac0b
Christian <christian@springscript.com>
2017-07-22 13:46:30 -0500

    removed filter for testing

--- src/android/effects.rs
+++ src/android/effects.rs
@@ -5,6 +5,7 @@
 
 rs_allocation yuv_in;
 rs_allocation kmeans_in;
+rs_allocation mAllocationTest;
 
 uint32_t width;
 uint32_t offset_to_u;
@@ -253,10 +254,12 @@ uchar4 __attribute__((kernel)) hysteresis(uint32_t x, uint32_t y) {
 	return black;
 }
 
-void uchar4 __attribute__((kernel)) kMeans(uchar4 in, uint32_t x, uint32_t y) {
+uchar4 __attribute__((kernel)) kMeans(uchar4 in, uint32_t x, uint32_t y) {
 	//uchar4 result;
 	
 	float pixel = getElementAt_uchar_to_float(in, x, y);
 	rsDebug("pixel: ", pixel);
+	
+	return mAllocationTest;
 }
 
