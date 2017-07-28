403f89797ec00bb12aec89335c33924d4fd4d069
Christian <christian@springscript.com>
2017-07-28 10:14:59 -0500

    an empy message

--- src/android/effects.rs
+++ src/android/effects.rs
@@ -328,7 +328,7 @@ int static getDistance(int i, uchar4 pixel) {
 	int gx = abs(Clusters[i].green - pixel.g);
 	int bx = abs(Clusters[i].blue - pixel.b);
 	int d = (rx+gx+bx) / 3;
-	rsDebug("distance: ", d);
+	rsDebug("distance: ", rx /t gx /t bx);
 	return d;
 }
 
