f3d644f27486c4f108613441d1476b76b99481e5
Christian <christian@springscript.com>
2017-07-28 11:09:00 -0500

    an empy message

--- src/android/effects.rs
+++ src/android/effects.rs
@@ -421,6 +421,8 @@ void kMeans(const uchar4* in, uchar4* out, uint32_t x, uint32_t y) {
    int cInt = findMinimalCluster(pixel);
    struct cluster Clusters[cInt];
    
+   rsDebug("fuck: ", Clusters[2].red);
+   
    int clusterId = width*y+x;
    //rsDebug("cluster id: ", Clusters[cInt].id);
    //rsDebug("x: ", x);
