513ddf94a7bc5a72e6b521485db5f8df099f6485
Christian <christian@springscript.com>
2017-08-03 10:15:50 -0500

    an empy message

--- src/android/effects.rs
+++ src/android/effects.rs
@@ -431,7 +431,7 @@ void kMeans(const uchar4* in, uchar4* out, uint32_t x, uint32_t y) {
    //rsDebug("fuck: ", Clusters[cInt].red);
    
    int clusterId = width*y+x;
-   rsDebug("Cluster[2]->red: ", *Clusters[2].red);
+   rsDebug("Cluster [2]->red: ", &Clusters[2].red);
    //rsDebug("lut->red: ", currentLUT.r);
    //rsDebug("x: ", x);
    //rsDebug("y: ", y);
