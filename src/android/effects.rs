8659efbd711793f0dcda0346712f0b6005abf9e6
Christian <christian@springscript.com>
2017-08-07 14:57:04 -0500

    blank message

--- src/android/effects.rs
+++ src/android/effects.rs
@@ -444,7 +444,7 @@ void __attribute__((kernel)) kMeans(uchar4 in, uint32_t x, uint32_t y) {
    //currentLUT    = rsGetElementAt_uchar4(lut, x, y);
    
 	//int lutTest = rsGetElementAt(lut, clusterId);
-	int lutTest = *lut[clusterId];
+	int32_t lutTest = *lut[clusterId];
    rsDebug("lutTest: ", lutTest);
    
    /*uchar addVal = 0;  
